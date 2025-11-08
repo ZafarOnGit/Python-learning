0c4610ebf35cdb1fa68d89c874d43ff0af927795
Mohamed Zafar <zafaer213133@gmail.com>
2025-11-08 11:23:48 +0400

    Fixed the problem with the code and solved the question

--- input-output.py
+++ input-output.py
@@ -1,13 +1,7 @@
 #Complete the function printNumber which takes an integer input from the user and prints it on the screen.
-'''class Solution {
-public:
-    void printNumber() {
-
-    }
-};'''
 
 class Solution:
     def printNumber(self):
-        x = int(input("Input(user gives value:"));
-        print(x);
-    printNumber()
+        n = int(input("Input(user gives value):"));
+        print(n);
+Solution().printNumber()
